4dade4a583cf84e7fe22148cf23111ab772bda50
bonnie <kusasen@gmail.com>
2021-07-18 11:17:47 +0800

    feat: add shop seeder

--- models/shop.js
+++ models/shop.js
@@ -4,6 +4,38 @@ const shopSchema = new Schema({
     name: {
         type: String,
         required: true,
+    },
+    name_en:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    location:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    },
+    google_map:{
+        type: String,
+        required: true,
+    },
+    rating:{
+        type: String,
+        required: false,
+    },
+    description:{
+        type: String,
+        required: false,
     }
 });
 
