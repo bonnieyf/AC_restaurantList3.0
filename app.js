3101c9bbc4875ed2190e3f73944fd9460f547d83
bonnie <kusasen@gmail.com>
2021-07-18 19:30:15 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars');
 const app = express();
 const post = 3000;
 
+const bodyParser = require('body-parser')
+const Shop = require('./models/shop');
+
 
 
 ////////////////////////////////
@@ -23,9 +26,40 @@ db.once('open',()=>{
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine','hbs');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res)=>{
-    res.send('index');
+    Shop.find()
+        .lean()
+        .then(shops => res.render('index', { shops }))
+        .catch(err => console.log(err));
 })
+
+app.get('/shops/new' , (req, res)=>{
+    return res.render('new');
+})
+
+app.post('/shops', (req, res)=>{
+    const body = req.body;
+    const data = {
+        name: body.name,
+        name_en: body.name_en,
+        category: body.category,
+        image: body.image,
+        location: body.location,
+        phone: body.phone,
+        google_map: body.google_map,
+        rating: body.rating,
+        description: body.description,
+    }
+    return Shop.create(data)
+        .then(()=> res.redirect('/'))
+        .catch(err => console.log(err));
+})
+
+
+
 
 app.listen(post ,()=>{
     console.log('App is running on http://localhost:3000')
